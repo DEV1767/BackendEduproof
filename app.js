a8761f3f9f93caec41580f4dd1ada7f0054d6617
SHIVAM CHAUDHARY <Shivamchy076@gmail.com>
2025-12-16 00:31:42 +0530

    Fix CORS origin URL by removing trailing slash

--- app.js
+++ app.js
@@ -12,7 +12,7 @@ app.use(cookieParser());
 app.use(cors({
     origin: [
         "http://localhost:5500",
-        "https://backend-eduproof.vercel.app/"
+        "https://backend-eduproof.vercel.app"
     ],
     credentials: true
 }));
