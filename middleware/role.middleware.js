1c564d2345018fc0b923ed110adfb8fe9fae6011
SHIVAM CHAUDHARY <Shivamchy076@gmail.com>
2025-12-16 22:36:14 +0530

    Delete middleware/role.middleware.js

--- middleware/role.middleware.js
+++
@@ -1,9 +0,0 @@
-import { Apierror } from "../utils/apierror";
-export const authorizeRoles = (...roles) => {
-    return (req, res, next) => {
-        if (!roles.includes(req.user.role)) {
-            throw new Apierror(403, "Access denied");
-        }
-        next();
-    };
-};
